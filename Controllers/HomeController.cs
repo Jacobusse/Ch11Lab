58f23809a58eda3f588eaa89d8de9a9438d626bc
Jacobusse <john.jacobusse@gmail.com>
2023-10-18 10:20:50 -0400

    Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Ch11Lab.Models;
+
+namespace Ch11Lab.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new UserTickets() { User = new User() });
+    }
+
+    [HttpPost]
+    public IActionResult Index(UserTickets userTickets) 
+    {
+        ModelState.Clear();
+        return View(userTickets);
+    }
+}
